d2fa9b88d8af3881e720fc815d99651f95d24783
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2021-12-16 16:20:30 +0100

    move throw to newline where applicable

--- src/index.js
+++ src/index.js
@@ -11,13 +11,19 @@ module.exports = (function () {
    * @param {any} options
    */
   const run = function (name, parameters, options) {
-    if (name === undefined) throw new Error('No name supplied!')
-    if (typeof name !== 'string' || name === null) throw new Error('name must be a string and must not be null!')
-    if (!name.match(/^[\w\-. ]+$/)) throw new Error('Name is invalid, should only contain letters, numbers or special characters: -._ or a space!')
+    if (name === undefined)
+      throw new Error('No name supplied!')
+    if (typeof name !== 'string' || name === null)
+      throw new Error('name must be a string and must not be null!')
+    if (!name.match(/^[\w\-. ]+$/))
+      throw new Error('Name is invalid, should only contain letters, numbers or special characters: -._ or a space!')
 
-    if (parameters === undefined) throw new Error('No parameters supplied!')
-    if (typeof parameters !== 'object' || parameters === null) throw new Error('parameters must be an object type array and must not be null!')
-    if (parameters && !Array.isArray(parameters)) throw new Error('parameters must be an array!')
+    if (parameters === undefined)
+      throw new Error('No parameters supplied!')
+    if (typeof parameters !== 'object' || parameters === null)
+      throw new Error('parameters must be an object type array and must not be null!')
+    if (parameters && !Array.isArray(parameters))
+      throw new Error('parameters must be an array!')
 
     const request = buildRequest(name, parameters, options)
 
@@ -84,13 +90,17 @@ module.exports = (function () {
    * @param {any} options 
    */
   const configure = function (options) {
-    if (options === undefined) throw new Error('No Options supplied!')
-    if (typeof options !== 'object' || options === null) throw new Error('Options must be an object and must not be null!')
+    if (options === undefined)
+      throw new Error('No Options supplied!')
+    if (typeof options !== 'object' || options === null)
+      throw new Error('Options must be an object and must not be null!')
 
-    if (Array.isArray(options)) throw new Error('Options must be an object and not an array!')
+    if (Array.isArray(options))
+      throw new Error('Options must be an object and not an array!')
 
     // Validate if options is an empty object
-    if (Object.keys(options).length === 0) throw new Error('Options must contain at least one property!')
+    if (Object.keys(options).length === 0)
+      throw new Error('Options must contain at least one property!')
 
     // validate parameters if not undefined and apply them
     if (options.username !== undefined) {
