c9378374ee97e6b7bc3bbef8798a7567c0a11ce1
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2021-12-17 10:51:02 +0100

    moved away from single export function

--- src/oe-param.js
+++ src/oe-param.js
@@ -4,93 +4,88 @@
 const validators = require('./validators').Validators
 
 /**
- * 
+ * Returns array of valid procedure parameters
+ * @param {any[]} parameters of the procedure
+ * @param {any} configuration
+ * @returns factory of valid procedure parameters
  */
-module.exports = (function () {
-  /**
-   * Returns array of valid procedure parameters
-   * @param {any[]} parameters of the procedure
-   * @param {any} configuration
-   * @returns factory of valid procedure parameters
-   */
-  const build = function (parameters, configuration) {
-    if (!validators.isArray(parameters))
-      throw new Error('parameters must be an array!')
-    
-    const parameterResult = []
+function build(parameters, configuration) {
+  if (!validators.isArray(parameters))
+    throw new Error('parameters must be an array!')
 
-    for (const [i, param] of parameters.entries()) {
-      let parameter
+  const parameterResult = []
 
-      if (param === undefined) {
-        parameter = getOutputParameter(i + 1, configuration)
-      } else {
-        parameter = getInputParameter(i + 1, param, configuration)
-      }
+  for (const [i, param] of parameters.entries()) {
+    let parameter
 
-      parameterResult.push(parameter)
+    if (param === undefined) {
+      parameter = getOutputParameter(i + 1, configuration)
+    } else {
+      parameter = getInputParameter(i + 1, param, configuration)
     }
 
-    return parameterResult
+    parameterResult.push(parameter)
   }
 
-  /**
-   * Return the output parameter object
-   * @param {number} index position of the parameter
-   * @param {any} configuration
-   * @returns output parameter object
-   */
-  const getOutputParameter = function (index, configuration) {
-    return {
-      pos: index,
-      out: true
-    }
-  }
+  return parameterResult
+}
 
-  /**
-   * Return the input parameter object for value
-   * @param {number} index position of the parameter
-   * @param {any} parameter value
-   * @param {any} configuration
-   * @returns input parameter object
-   */
-  const getInputParameter = function (index, param, configuration) {
-    const parameter = {
-      pos: index,
-      value: param,
-      type: resolveParameterType(param, configuration)
-    }
-
-    if (!parameter.type) {
-      delete parameter.type
-    }
-
-    return parameter
-  }
-
-  /**
-   * return the type of parameter supplied as input, if default return undefined
-   * number should be 'integer' and object should be 'json'
-   * @param {any} param 
-   * @param {any} configuration
-   * @returns {string} return the type of the parameter
-   */
-  const resolveParameterType = function (param, configuration) {
-    const paramType = typeof param
-    switch (paramType) {
-      case configuration.parameterDefaults.in:
-        return
-
-      case 'number':
-        return 'integer'
-
-      case 'object':
-        return 'json'
-    }
-    return paramType
-  }
-
+/**
+ * Return the output parameter object
+ * @param {number} index position of the parameter
+ * @param {any} configuration
+ * @returns output parameter object
+ */
+function getOutputParameter(index, configuration) {
   return {
-    build
+    pos: index,
+    out: true
   }
-})()
+}
+
+/**
+ * Return the input parameter object for value
+ * @param {number} index position of the parameter
+ * @param {any} parameter value
+ * @param {any} configuration
+ * @returns input parameter object
+ */
+function getInputParameter(index, param, configuration) {
+  const parameter = {
+    pos: index,
+    value: param,
+    type: resolveParameterType(param, configuration)
+  }
+
+  if (!parameter.type) {
+    delete parameter.type
+  }
+
+  return parameter
+}
+
+/**
+ * return the type of parameter supplied as input, if default return undefined
+ * number should be 'integer' and object should be 'json'
+ * @param {any} param 
+ * @param {any} configuration
+ * @returns {string} return the type of the parameter
+ */
+function resolveParameterType(param, configuration) {
+  const paramType = typeof param
+  switch (paramType) {
+    case configuration.parameterDefaults.in:
+      return
+
+    case 'number':
+      return 'integer'
+
+    case 'object':
+      return 'json'
+  }
+  return paramType
+}
+
+module.exports = {
+  build
+}
