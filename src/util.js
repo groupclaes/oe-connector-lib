83b41c94b0f75bad2b9ec7f89aa4906971812eab
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2021-12-17 10:04:51 +0100

    Revert "refactor getEnvVariable add validateRequest" This reverts commit 9b3dec7b80493e909751f29da6f0879954d7c9d3.

--- src/util.js
+++ src/util.js
@@ -4,26 +4,28 @@
 const validators = require('./validators')
 
 module.exports = (function () {
-  const validateRequest = function (name) {
+  /**
+   * Retrieves value from environment variable if set, otherwise return defaultValue
+   * @param {string} name of the environmnet variable 
+   * @param {any} defaultValue which should be used if variable is not set
+   * @returns 
+   */
+  const getEnvVariable = function (name, defaultValue) {
     if (validators.isUndefined(name))
       throw new Error('Name must be supplied!')
     if (!validators.isString(name))
       throw new Error('Name must be a string!')
+
+    const value = process.env[name]
+
+    if (validators.isString(value)) {
+      return value
+    }
+
+    return defaultValue
   }
 
   return {
-    getEnvVariable: function (name, defaultValue) {
-      validateRequest(name)
-      const value = process.env[name]
-
-      return validators.isString(value) ? value : defaultValue
-    }
+    getEnvVariable
   }
 })()
-
-/**
- * Retrieves value from environment variable if set, otherwise return defaultValue
- * @param {string} name of the environmnet variable 
- * @param {any} defaultValue which should be used if variable is not set
- * @returns 
- */
