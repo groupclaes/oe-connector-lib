4050a6783ff81d7100f7e3abb86aa29740911d11
ThibautNijsPXl <thibaut.nijs2@student.pxl.be>
2021-12-21 16:52:53 +0100

    Rewrite unit tests for validators

--- src/validators.test.js
+++ src/validators.test.js
@@ -1,115 +1,128 @@
 const Validators = require('./validators')
+const { describe, expect, test } = require('@jest/globals')
 
-test('Validators.isUndefined()', () => {
-  expect(Validators.isUndefined(undefined)).toBe(true)
+describe('Validators.isUndefined()', () => {
+  test('Should return true when testing undefined', () => {
+    expect(Validators.isUndefined(undefined)).toBe(true)
+  })
 
-  expect(Validators.isUndefined(null)).toBe(false)
-  expect(Validators.isUndefined(0)).toBe(false)
-  expect(Validators.isUndefined(true)).toBe(false)
-  expect(Validators.isUndefined(false)).toBe(false)
-  expect(Validators.isUndefined('foo')).toBe(false)
-  expect(Validators.isUndefined({})).toBe(false)
-  expect(Validators.isUndefined([])).toBe(false)
+  const cases = [ [null], [0], [true], [false], ['foo'], [{}], [[]] ]
+  test.each(cases)('Should return false when providing %s argument', (argument) => {
+    expect(Validators.isUndefined(argument)).toBe(false)
+  })
 })
 
-test('Validators.isNull()', () => {
-  expect(Validators.isNull(null)).toBe(true)
+describe('Validators.isNull()', () => {
+  test('Should return true when testing null', () => {
+    expect(Validators.isNull(null)).toBe(true)
+  })
 
-  expect(Validators.isNull(undefined)).toBe(false)
-  expect(Validators.isNull(0)).toBe(false)
-  expect(Validators.isNull(true)).toBe(false)
-  expect(Validators.isNull(false)).toBe(false)
-  expect(Validators.isNull('foo')).toBe(false)
-  expect(Validators.isNull({})).toBe(false)
-  expect(Validators.isNull([])).toBe(false)
+  const cases = [ [undefined], [0], [true], [false], ['foo'], [{}], [[]] ]
+  test.each(cases)('Should return false when providin %p argument', (argument) => {
+    expect(Validators.isNull(argument)).toBe(false)
+  })
 })
 
-test('Validors.isString()', () => {
-  expect(Validators.isString('')).toBe(true)
-  expect(Validators.isString('undefined')).toBe(true)
+describe('Validators.isString()', () => {
+  const trueCases = [ [''], ['undefined'], ['1'] ]
+  test.each(trueCases)('Should return true when testing %p', (argument) => {
+    expect(Validators.isString(argument)).toBe(true)
+  })
 
-  expect(Validators.isString(undefined)).toBe(false)
-  expect(Validators.isString()).toBe(false)
-  expect(Validators.isString(null)).toBe(false)
-  expect(Validators.isString(0)).toBe(false)
-  expect(Validators.isString(1)).toBe(false)
-  expect(Validators.isString(true)).toBe(false)
-  expect(Validators.isString(false)).toBe(false)
+  const cases = [ [undefined], [0], [1], [true], [false], [{}], [[]] ]
+  test.each(cases)('Should return false when testing %p argument', (argument) => {
+    expect(Validators.isString(argument)).toBe(false)
+  })
 })
 
-test('Validors.isNumber()', () => {
-  expect(Validators.isNumber(0)).toBe(true)
-  expect(Validators.isNumber(0x43)).toBe(true)
+describe('Validators.isNumber()', () => {
+  const trueCases = [ [0], [0x43], [0.1], [.1] ]
+  test.each(trueCases)('Should return true when testing %p', (argument) => {
+    expect(Validators.isNumber(argument)).toBe(true)
+  })
 
-  expect(Validators.isNumber(undefined)).toBe(false)
-  expect(Validators.isNumber(null)).toBe(false)
-  expect(Validators.isNumber('')).toBe(false)
-  expect(Validators.isNumber()).toBe(false)
-  expect(Validators.isNumber(true)).toBe(false)
-  expect(Validators.isNumber(false)).toBe(false)
-  expect(Validators.isNumber('0')).toBe(false)
-  expect(Validators.isNumber('0x43')).toBe(false)
+  const cases = [ [undefined], [null], [''], [true], [false], ['0'], ['0x43'], ['0.1'], ['.1'] ]
+  test.each(cases)('Should return false when testing %p', (argument) => {
+    expect(Validators.isNumber(argument)).toBe(false)
+  })
 })
 
-test('Validors.isBoolean()', () => {
-  expect(Validators.isBoolean(true)).toBe(true)
-  expect(Validators.isBoolean(false)).toBe(true)
+describe('Validators.isBoolean()', () => {
+  const trueCases = [ [true], [false] ]
+  test.each(trueCases)('Should return true when testing %p', (argument) => {
+    expect(Validators.isBoolean(argument)).toBe(true)
+  })
 
-  expect(Validators.isBoolean(undefined)).toBe(false)
-  expect(Validators.isBoolean(null)).toBe(false)
-  expect(Validators.isBoolean('')).toBe(false)
-  expect(Validators.isBoolean()).toBe(false)
-  expect(Validators.isBoolean('true')).toBe(false)
-  expect(Validators.isBoolean('false')).toBe(false)
-  expect(Validators.isBoolean(0)).toBe(false)
-  expect(Validators.isBoolean(0x43)).toBe(false)
+  const cases = [ [undefined], [null], [''], ['true'], ['false'], ['0'], ['0x43'], [0], [1] ]
+  test.each(cases)('Should return false when testing %s', (argument) => {
+    expect(Validators.isBoolean(argument)).toBe(false)
+  })
 })
 
-test('Validors.isObject()', () => {
-  expect(Validators.isObject({})).toBe(true)
-  expect(Validators.isObject([])).toBe(true)
-  expect(Validators.isObject({ foo: 'bar' })).toBe(true)
-  expect(Validators.isObject(null)).toBe(true)
+describe('Validators.isObject()',  () => {
+  const trueCases = [ [{}], [[]], [{ foo: 'bar' }], null ]
+  test.each(trueCases)('Should return true when testing %p', (argument) => {
+    expect(Validators.isObject(argument)).toBe(true)
+  })
 
-  expect(Validators.isObject(undefined)).toBe(false)
-  expect(Validators.isObject('')).toBe(false)
-  expect(Validators.isObject()).toBe(false)
-  expect(Validators.isObject('true')).toBe(false)
-  expect(Validators.isObject('false')).toBe(false)
-  expect(Validators.isObject(0)).toBe(false)
-  expect(Validators.isObject(0x43)).toBe(false)
+  const cases = [ [undefined], [''], ['true'], ['false'], [true], [false], ['0'], ['0x43'], [0], [1] ]
+  test.each(cases)('Should return false when testing %s', (argument) => {
+    expect(Validators.isObject(argument)).toBe(false)
+  })
 })
 
-test('Validors.isArray()', () => {
-  expect(Validators.isArray([0, 1])).toBe(true)
-  expect(Validators.isArray([])).toBe(true)
-  expect(Validators.isArray([,])).toBe(true)
+describe('Validators.isArray()',  () => {
+  const trueCases = [ [[0, 1]], [[]], [[,]] ]
+  test.each(trueCases)('Should return true when testing %p', (argument) => {
+    expect(Validators.isArray(argument)).toBe(true)
+  })
 
-  expect(Validators.isArray(undefined)).toBe(false)
-  expect(Validators.isArray(null)).toBe(false)
-  expect(Validators.isArray('')).toBe(false)
-  expect(Validators.isArray()).toBe(false)
-  expect(Validators.isArray({})).toBe(false)
-  expect(Validators.isArray({ foo: 'bar' })).toBe(false)
-  expect(Validators.isArray()).toBe(false)
-  expect(Validators.isArray('true')).toBe(false)
-  expect(Validators.isArray('false')).toBe(false)
-  expect(Validators.isArray(0)).toBe(false)
-  expect(Validators.isArray(0x43)).toBe(false)
+  const cases = [ [undefined], [null], [''], [true], [false], ['true'], ['false'], ['0'], ['0x43'], [0], [1], [0x43], [{}], [{ foo: 'bar' }] ]
+  test.each(cases)('Should return false when testing %s', (argument) => {
+    expect(Validators.isArray(argument)).toBe(false)
+  })
 })
 
-test('Validors.isBetween()', () => {
-  expect(Validators.isBetween(10, 0, 100)).toBe(true)
-  expect(Validators.isBetween(1, 1, 1)).toBe(true)
+describe('Validators.isBetween()', () => {
+  const trueCases = [
+    [10, 0, 100],
+    [1, 1, 1],
+    ['1', 0, 100],
+    ['2', 0, 100]
+  ]
+  test.each(trueCases)('Should return true when testing %p', (value, min, max) => {
+    expect(Validators.isBetween(value, min, max)).toBe(true)
+  })
 
-  expect(Validators.isBetween(-1, 0, 100)).toBe(false)
-  expect(Validators.isBetween(101, 0, 100)).toBe(false)
+  const cases = [
+    [-1, 0, 100],
+    [101, 0, 100],
+    [1, 0, 0],
+    [-1, 0, 100],
+  ]
+  test.each(cases)('Should return false when testing %s', (value, min, max) => {
+    expect(Validators.isBetween(value, min, max)).toBe(false)
+  })
 })
 
-test('Validors.isNotBetween()', () => {
-  expect(Validators.isNotBetween(-1, 0, 100)).toBe(true)
-  expect(Validators.isNotBetween(101, 0, 100)).toBe(true)
+describe('Validators.isNotBetween()', () => {
+  const trueCases = [
+    [-1, 0, 100],
+    [101, 0, 100],
+    [1, 0, 0],
+    [-1, 0, 100],
+  ]
+  test.each(trueCases)('Should return true when testing %p', (value, min, max) => {
+    expect(Validators.isNotBetween(value, min, max)).toBe(true)
+  })
 
-  expect(Validators.isNotBetween(10, 0, 100)).toBe(false)
-  expect(Validators.isNotBetween(1, 1, 1)).toBe(false)
+  const cases = [
+    [10, 0, 100],
+    [1, 1, 1],
+    ['1', 0, 100],
+    ['2', 0, 100]
+  ]
+  test.each(cases)('Should return false when testing %s', (value, min, max) => {
+    expect(Validators.isNotBetween(value, min, max)).toBe(false)
+  })
 })
