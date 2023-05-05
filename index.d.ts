6b3efa880367e58c21e213fcaf56c7c1589fd539
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2023-05-05 11:56:57 +0200

    options is not required

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 declare class OE {
   configuration: IConfiguration
 
-  run(name: string, parameters: any[], options: IConfiguration): Promise<any>
-  test(name: string, parameters: any[], options: IConfiguration): any
+  run(name: string, parameters: any[], options?: IConfiguration): Promise<any>
+  test(name: string, parameters: any[], options?: IConfiguration): any
   configure(options: IConfiguration): void
 }
 
